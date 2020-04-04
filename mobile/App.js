490902b1960901dd746f24917fcd876d9bfd2f1d
Henrique Almeida de Moura <henrique.almeida@ecommercebrasil.com.br>
2020-04-04 01:58:33 -0300

    Feat(Mobile): Finished Mobile App in React Native, Front view, Comunication with API, routes and other thing React components

---
+++ mobile/App.js
@@ -0,0 +1,15 @@
+import 'intl'
+import 'intl/locale-data/jsonp/pt-BR'
+
+import React from 'react';
+
+import { StyleSheet, Text, View } from 'react-native';
+
+import Routes from './src/routes'
+export default function App() {
+  return (
+    <Routes/>
+  );
+}
+
+
